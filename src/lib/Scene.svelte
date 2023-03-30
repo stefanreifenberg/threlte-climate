7c3bb473e2be0fb50c8af08f15e05cbae04dc7c0
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2023-03-30 10:41:47 +0200

    fix data sorting, fix y position, add labels

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -1,6 +1,6 @@
 <script>
     import { T } from '@threlte/core'
-    import { OrbitControls } from '@threlte/extras'
+    import { OrbitControls, HTML } from '@threlte/extras'
     import { autoType, csvParse, extent, scaleLinear } from 'd3';
     import { onMount } from 'svelte';
 
@@ -10,11 +10,10 @@
         .domain(extent(data, d => d.MEAN))
         .range(["#0000ff", "#ff0000"])
 
-
-    // position the cubes along a circle where y has to always be 0 
-    $: position = (i,d) => {
+    // position the cubes along a circle the y position should result in 0 taking d into account
+    $: position = (i, d) => {
         const angle = (i / data.length) * Math.PI * 2;
-        return [Math.cos(angle) * 5, 0, Math.sin(angle) *5];
+        return [Math.cos(angle) * 5, 0 + Math.abs(d), Math.sin(angle) * 5];
     }
 
     // rotate the cubes so that they are always facing the center
@@ -23,14 +22,14 @@
         return [0, - angle, 0];
     }
 
-    $: console.log(position(1));
-
     async function load() {
         const climate_data= `/global-average-air-temperature-anomalies-5.csv`;
         const response = await fetch(climate_data);
 
         if(response.ok) {
             data = csvParse(await response.text(), autoType);
+            // arrange the data by Year
+            data.sort((a, b) => a.Year - b.Year);
         }
     }
 
@@ -44,15 +43,27 @@
 <!-- create a box geometry for every data point in data -->
 {#if data}
     {#each data as d, i}
-        <T.Mesh position={position(i,d)} scale.y={1 + 15 * d.MEAN}  rotation={rotation(i)} castShadow receiveShadow>
-            <T.BoxGeometry args={[0.15, 0.15, 0.15]} />
+        <T.Mesh position={position(i,d.MEAN)}  rotation={rotation(i)} castShadow receiveShadow>
+            <T.BoxGeometry args={[0.17, 2 * d.MEAN, 0.17]} />
             <T.MeshStandardMaterial color={colorScale(d.MEAN)} />
         </T.Mesh>
+
+        {#if i % 20 === 0}
+            <HTML position={position(i, 1)} >
+                <h1 class="label">{d.Year}</h1>
+            </HTML>
+
+            <T.Mesh position={position(i, 0.5)} rotation={rotation(i)} castShadow receiveShadow>
+                <T.CylinderGeometry args={[0.01, 0.01, 1, 32]} />
+                <T.MeshStandardMaterial color="#000000" />
+            </T.Mesh>
+        {/if}
     {/each}
 {/if}
 
+
 <T.PerspectiveCamera
-  position={[0, 20, 20]}
+  position={[0, 10, 10]}
   makeDefault
   fov={50}
 >
@@ -60,14 +71,14 @@
 </T.PerspectiveCamera>
 
 <!-- add an ambientlight -->
-<T.AmbientLight intensity={0.5} />
+<T.AmbientLight intensity={0.7} />
 
 <!-- add a directional light -->
-<T.DirectionalLight position={[3, 10, 10]} intensity={3} castShadow />
-<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} castShadow/>
+<T.DirectionalLight position={[3, 10, 10]} intensity={0.2} castShadow />
+<T.DirectionalLight position={[-3, 10, -10]} intensity={1} castShadow/>
 
 <!-- create a round floor geometry -->
-<T.Mesh position={[0, -2, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-  <T.CircleGeometry args={[10, 32]} />
-  <T.MeshStandardMaterial color="#c0c0c0" />
+<T.Mesh position={[0, -0.001, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+  <T.CircleGeometry args={[15, 32]} />
+  <T.MeshStandardMaterial />
 </T.Mesh>
